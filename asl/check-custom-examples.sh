2761bd521ac6f445460e952884d199430313feda
andugu <andugu@users.noreply.github.com>
2020-06-05 15:52:33 +0200

    READY 👨🏻‍💻

--- asl/check-custom-examples.sh
+++ asl/check-custom-examples.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/bash
+#!/bin/zsh
 
-echo "BEGIN els meus putos tests"
+echo "BEGIN Running Custom Tests"
 for f in ../examples/jp_genc_*.asl; do
 	rm ../tvm/test.t
-    echo $(basename "$f")
+    echo "[*]" $(basename "$f")
     ./asl "$f"  > ../tvm/test.t
     ../tvm/tvm ../tvm/test.t < "${f/asl/in}" > ../tvm/out.txt 
     diff "${f/asl/out}" ../tvm/out.txt
 done
-echo "END els meus putos tests"
+echo "END Running Custom Tests"
